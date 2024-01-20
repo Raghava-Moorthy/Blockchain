8ed1290c11687afeafbacae692cf1e57feac1181
Raghava <moorthy46030@gmail.com>
2024-01-20 12:29:59 +0530

    change

--- merkletree/merkle.js
+++ merkletree/merkle.js
@@ -1,4 +1,4 @@
-import { createHash } from "crypto";
+const crypto = require("crypto");
 
 class MerkleTree {
   constructor(transactions) {
@@ -7,18 +7,19 @@ class MerkleTree {
     this.branch = this.build();
     this.root = this.branch[0];
   }
-  hashing = data => createHash("SHA256").update(data).digest("hex");
+  hashing = (data) => crypto.createHash("SHA256").update(data).digest("hex");
 
-  lengthCheck = total => {
+  lengthCheck = (total) => {
     if (total.length % 2 == 1) total.push(total[total.length - 1]);
-    return total.slice();
+    return total;
   };
 
-  leafHash = treeNode => {
+  leafHash = (treeNode) => {
     let nodes = [];
     treeNode = this.lengthCheck(treeNode);
-    for (let i = 0; i < treeNode.length; i += 2)
+    for (let i = 0; i < treeNode.length; i += 2) 
       nodes.push(this.hashing(treeNode[i] + treeNode[i + 1]));
+    
     return nodes;
   };
 
@@ -26,19 +27,20 @@ class MerkleTree {
     let leaf = this.transaction.map((val) => this.hashing(val));
     leaf = this.lengthCheck(leaf);
 
-    let treeNode = [...leaf];
+    let treeNode = leaf.slice();
 
     while (treeNode.length > 1) {
-      this.treeVal.push(...treeNode);
+      this.treeVal.push(treeNode.slice());
       treeNode = this.leafHash(treeNode);
     }
+    this.treeVal.push(treeNode); 
     return treeNode;
   };
 
-  verification = (transaction) => {
+  validation = (transaction) => {
     let node = this.hashing(transaction);
     for (let i = 0; i < this.treeVal.length; i++)
-      if (this.treeVal[i] == node) return "Transaction Found";
+        if (this.treeVal[0][i] == node) return "Transaction Found";
 
     return "Transaction Not Found";
   };
@@ -46,12 +48,27 @@ class MerkleTree {
   merkleRoot = () => this.root;
 }
 
-let arr = ["txt1", "txt2", "txt3", "txt4"];
+let arr = ["1", "2", "3", "4","8","7","20"];
 const merkleTree = new MerkleTree(arr);
 
-let input1 = "txt6";
-let input2 = "txt3";
+let input1 = "6";
+let input2 = "3";
 
 console.log("\nRoot => ", merkleTree.merkleRoot());
-console.log(`\n ${input1} - ${merkleTree.verification(input1)} `);
-console.log(`\n ${input2} - ${merkleTree.verification(input2)} \n`);
+console.log(`\n ${input1} - ${merkleTree.validation(input1)}`);
+console.log(`\n ${input2} - ${merkleTree.validation(input2)} \n`);
+
+merkleTree.treeVal.reverse();
+let val = merkleTree.treeVal;
+let co = 1;
+
+for (let i = 0; i < val.length; i++) {
+  console.log(`Level ${co}\n`);
+
+  for (let j = 0; j < val[i].length; j++) {
+    console.log(`${val[i][j]}\n`);
+  }
+
+  console.log("\n");
+  co++;
+}
